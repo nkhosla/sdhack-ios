a0f767c3cba906e1356ea9e46cc7700871536bc6
Nathan khosla <nathan@nkhosla.com>
2017-06-24 14:57:38 -0700

    Initial Commit

---
+++ AutoText/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AutoText
+//
+//  Created by Nathan khosla on 24 Jun 17.
+//  Copyright © 2017 Nathan khosla. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AutoTextTests/AutoTextTests.swift
@@ -0,0 +1,36 @@
+//
+//  AutoTextTests.swift
+//  AutoTextTests
+//
+//  Created by Nathan khosla on 24 Jun 17.
+//  Copyright © 2017 Nathan khosla. All rights reserved.
+//
+
+import XCTest
+@testable import AutoText
+
+class AutoTextTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
