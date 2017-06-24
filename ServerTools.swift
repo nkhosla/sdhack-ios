298e99182b962fc7741b0b42a68c476a3239a283
Nathan khosla <nathan@nkhosla.com>
2017-06-24 16:40:11 -0700

    adde a servtertools class

---
+++ ServerTools.swift
@@ -0,0 +1,13 @@
+//
+//  ServerTools.swift
+//  AutoText
+//
+//  Created by Nathan khosla on 24 Jun 17.
+//  Copyright © 2017 Nathan khosla. All rights reserved.
+//
+
+import UIKit
+
+class ServerTools: NSObject {
+
+}
