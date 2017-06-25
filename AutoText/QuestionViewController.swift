484241709fd4581bd5c792808effc40b67edd600
Nathan khosla <nathan@nkhosla.com>
2017-06-24 18:24:31 -0700

    removed expandingtableview and some UI

---
+++ AutoText/QuestionViewController.swift
@@ -0,0 +1,53 @@
+//
+//  QuestionViewController.swift
+//  AutoText
+//
+//  Created by Nathan khosla on 24 Jun 17.
+//  Copyright © 2017 Nathan khosla. All rights reserved.
+//
+
+import UIKit
+import ExpandingTableView
+
+class QuestionViewController: UITableViewController {
+    
+    @IBOutlet var questionTableView: UITableView!
+    
+    var questionList = ["When are we meeting tonight?",
+                        "What is the address for dinner?",
+                        "What time is dinner?",
+                        "Where are we going?"];
+    
+    let newQuestionDelay = 15;
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        //TODO add a timer to birng a new question in newQuestioDelay seconds
+        
+        let nib = UINib(nibName: "QuestionTableViewCell", bundle: nil)
+        
+        tableView.register(nib, forCellReuseIdentifier: "questioncell")
+
+    }
+ 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questionList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = questionTableView.dequeueReusableCell(withIdentifier: "questioncell", for: indexPath) as! QuestionTableViewCell
+        cell.questionLabel.text = questionList[indexPath.row]
+        return cell;
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100;
+    }
+}
+
+
