06882ab943efbcab723fadc3233afc11b2784747
Nathan khosla <nathan@nkhosla.com>
2017-06-25 01:30:57 -0700

    basically copied all fo the Demo classes from ChattoApp (the demo app provided with Chatto frameowrk) to make the message UI work, since there is no need for us to style our own

--- AutoText/ChatViewController.swift
+++ AutoText/ChatViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Nathan khosla on 25 Jun 17.
 //
-//
+// Shamelessly copied from the Chatto Demo Project
 
 import UIKit
 import Chatto
@@ -12,6 +12,92 @@ import ChattoAdditions
 
 class ChatViewController: BaseChatViewController {
 
+        var messageSender: FakeMessageSender!
+        var dataSource: FakeDataSource! {
+            didSet {
+                self.chatDataSource = self.dataSource
+            }
+        }
+        
+        lazy private var baseMessageHandler: BaseMessageHandler = {
+            return BaseMessageHandler(messageSender: self.messageSender)
+        }()
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            let image = UIImage(named: "bubble-incoming-tail-border", in: Bundle(for: ChatViewController.self), compatibleWith: nil)?.bma_tintWithColor(.blue)
+            super.chatItemsDecorator = ChatItemsDemoDecorator()
+            let addIncomingMessageButton = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(ChatViewController.addRandomIncomingMessage))
+            self.navigationItem.rightBarButtonItem = addIncomingMessageButton
+        }
+    
+        @objc
+        private func addRandomIncomingMessage() {
+            self.dataSource.addRandomIncomingMessage()
+        }
+        
+        var chatInputPresenter: BasicChatInputBarPresenter!
+        override func createChatInputView() -> UIView {
+            let chatInputView = ChatInputBar.loadNib()
+            var appearance = ChatInputBarAppearance()
+            appearance.sendButtonAppearance.title = NSLocalizedString("Send", comment: "")
+            appearance.textInputAppearance.placeholderText = NSLocalizedString("Type a message", comment: "")
+            self.chatInputPresenter = BasicChatInputBarPresenter(chatInputBar: chatInputView, chatInputItems: self.createChatInputItems(), chatInputBarAppearance: appearance)
+            chatInputView.maxCharactersCount = 1000
+            return chatInputView
+        }
+        
+        override func createPresenterBuilders() -> [ChatItemType: [ChatItemPresenterBuilderProtocol]] {
+            
+            let textMessagePresenter = TextMessagePresenterBuilder(
+                viewModelBuilder: DemoTextMessageViewModelBuilder(),
+                interactionHandler: DemoTextMessageHandler(baseHandler: self.baseMessageHandler)
+            )
+            textMessagePresenter.baseMessageStyle = BaseMessageCollectionViewCellAvatarStyle()
+            
+            let photoMessagePresenter = PhotoMessagePresenterBuilder(
+                viewModelBuilder: DemoPhotoMessageViewModelBuilder(),
+                interactionHandler: DemoPhotoMessageHandler(baseHandler: self.baseMessageHandler)
+            )
+            photoMessagePresenter.baseCellStyle = BaseMessageCollectionViewCellAvatarStyle()
+            
+            return [
+                DemoTextMessageModel.chatItemType: [
+                    textMessagePresenter
+                ],
+                DemoPhotoMessageModel.chatItemType: [
+                    photoMessagePresenter
+                ],
+                SendingStatusModel.chatItemType: [SendingStatusPresenterBuilder()],
+                TimeSeparatorModel.chatItemType: [TimeSeparatorPresenterBuilder()]
+            ]
+        }
+        
+        func createChatInputItems() -> [ChatInputItemProtocol] {
+            var items = [ChatInputItemProtocol]()
+            items.append(self.createTextInputItem())
+            items.append(self.createPhotoInputItem())
+            return items
+        }
+        
+        private func createTextInputItem() -> TextChatInputItem {
+            let item = TextChatInputItem()
+            item.textInputHandler = { [weak self] text in
+                self?.dataSource.addTextMessage(text)
+            }
+            return item
+        }
+        
+        private func createPhotoInputItem() -> PhotosChatInputItem {
+            let item = PhotosChatInputItem(presentingController: self)
+            item.photoInputHandler = { [weak self] image in
+                self?.dataSource.addPhotoMessage(image)
+            }
+            return item
+        }
+    }
+
+/*
     override func viewDidLoad() {
         super.viewDidLoad()
         self.chatDataSource = YourDataSource()
@@ -87,5 +173,7 @@ class ChatViewController: BaseChatViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    */
 
-}
+
